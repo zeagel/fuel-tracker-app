9d02e5cdf5f87e94d63a74a5c3ddf352b14a33f9
Mika Honkanen <mika.honkanen@gmail.com>
2021-01-03 16:26:17 +0200

    Config updates: - Remove unnecessary console.logs from config.js - Add support for running backend tests in quiet mode - Add description on frontend package.json

--- fuel-tracker-be/src/utils/config.js
+++ fuel-tracker-be/src/utils/config.js
@@ -1,18 +1,14 @@
 require('dotenv').config();
 
 let PORT = process.env.PORT || 3001;
-console.log('Config, PORT:', PORT);
 
 let MONGODB_URI = process.env.MONGODB_URI;
-console.log('Config, MONGODB_URI:', MONGODB_URI);
 
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
-  console.log('Config, MONGODB_URI (test):', MONGODB_URI);
 }
 
 let JWT_SECRET = process.env.JWT_SECRET;
-console.log('Config, JWT_SECRET:', JWT_SECRET);
 
 module.exports = {
   MONGODB_URI,
